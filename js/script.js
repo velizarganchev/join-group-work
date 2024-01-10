cd8af71a10db64b82d7f47f917e82b41dc00c1f7
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-01-10 01:25:20 +0100

    Added navigation bars

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const STORAGE_TOKEN = 'DR6FZK1MTGPR11C93C73PUGXTKY05AJ4CNFZMV8P';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
+
+
+async function init(id) {
+    await includeHTML();
+    changeNavigationHighlight(id);
+}
+
+
+/**
+ * Renders dynamic content in into the base html structures.
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -13,4 +24,65 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+}
+
+
+/**
+ * Toggles a class.
+ * @param {string} id - The id of the element which contains the class.
+ * @param {string} classname - The name of the class to be toggled.
+ */
+function toggleClass(id, classname) {
+    document.getElementById(id).classList.toggle(classname);
+}
+
+
+/**
+ * Changes the highlighted navigation element, depending on which site you are on.
+ * @param {string} id - The id of the navigation element to be highlighted.
+ */
+function changeNavigationHighlight(id) {
+    let highlighted = document.querySelector('.active');
+    highlighted.classList.remove('active');
+    let newHighlighted = document.getElementById(id);
+    checkIfOnHelpSite(id, newHighlighted)
+}
+
+
+/**
+ * Checks wheter or not the help page is open.
+ * @param {string} id - The id of the navigation element to be highlighted.
+ * @param {element} newHighlighted - The navigation element to be highlighted.
+ */
+function checkIfOnHelpSite(id, newHighlighted) {
+    if (newHighlighted.firstElementChild.src !== `${getProtocol()}//${getHost()}/assets/img/question-mark-icon.png`) {
+        newHighlighted.classList.add('active');
+        newHighlighted.firstElementChild.src = `${getProtocol()}//${getHost()}/assets/img/${id}-icon-white.png`;
+    }
+}
+
+
+/**
+ * Identifies the protocol method of the current site.
+ * @returns The protocol method as a string.
+ */
+function getProtocol() {
+    let protocol = window.location.protocol;
+    return protocol;
+}
+
+
+/**
+ * Identifies the host of the current site.
+ * @returns The host as a string.
+ */
+function getHost() {
+    let host = window.location.host;
+    return host;
+}
+
+
+
+function changeImageOnHover() {
+    
 }
