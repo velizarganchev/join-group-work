edef40682d2897455f8528af4f97e0240951b39f
unknown <j.quabeck@gmx.net>
2024-01-12 15:21:42 +0100

    working priority-section

--- js/task_form.js
+++ js/task_form.js
@@ -1,45 +1,73 @@
-let alreadychosen = false; // Var for choosing the Priority
+let currentchosenPrio; // needed in function "changeItoPrioInString" to see which Prio was chosen
 
-/** reads out which priority was chosen
- * @param {int} i - 1 = High Prio ; 2 = Medium Prio ; 3 = Low Prio
- * @param {String} Prio - Safes the Prio in words instead of numbers
+/** changes the HTML back to standard (=no Prio chosen)
+ * 
+ */
+function resetPrio(){
+    document.getElementById('SubcontentPrio').innerHTML = `
+        <div id="Prio1" class="prioclass hover" onclick="choosePrio(1)">
+            <div>Urgent</div>
+            <img id="HighImg" src="/assets/img/PrioHigh.png">
+        </div>
+        <div id="Prio2" class="prioclass hover" onclick="choosePrio(2)">
+            <div>Medium</div>
+            <img id="MediumImg" src="/assets/img/PrioMedium.png">
+        </div>
+        <div id="Prio3" class="prioclass hover" onclick="choosePrio(3)">
+            <div>Low</div>
+            <img id="LowImg" src="/assets/img/PrioLow.png">
+        </div>
+    `;
+}
+
+/**
+ * 
+ * @param {string} Prio - changes the given number i to a Prioity in string-form
+ * @param {int} i - gives the function the Prio in int (1 = Urgent / 2 = Medium / 3 = Low Priority)
+ * @param {int} currentchosenPrio - safe the current chosen Priority in int (like above)
+ */
+function changeItoPrioInString(Prio, i){
+    if (currentchosenPrio == i){
+        currentchosenPrio = 0;
+    }else{
+        if (i == 1){
+            Prio = 'High';
+        }else if(i == 2){
+            Prio = 'Medium';
+        }else {
+            Prio = 'Low';
+        }
+        currentchosenPrio = i;
+        changeColorOfPrio(Prio, i);
+    }
+}
+
+/** Reset the HTML and changes the color and the image for the chosen Prio
+ * 
+ * @param {int} i - gives the function the Prio in int (1 = Urgent / 2 = Medium / 3 = Low Priority)
  */
 function choosePrio(i){
+    resetPrio();
     let Prio;
-    if (i == 1){
-        Prio = 'High';
-    } else if(i == 2){
-        Prio = 'Medium';
-    } else{
-        Prio = 'Low';
-    }
-    changeColorOfPrio(Prio, i);
+    changeItoPrioInString(Prio, i);
 }
 
-/**changes the Img of the Prio div
+/** changes the img for the chosen Prio
  * 
- * @param {*} Prio - still the Priority in numbers
- * @param {*} img1 - the url of the image when not chosen
- * @param {*} img2 - the url of the image when chosen
+ * @param {string} Prio - safes the chosen Priority in string-form
  */
-function changeImgOfPrio(Prio, img1, img2){
-    if (alreadychosen == false){
-        document.getElementById(`${Prio}Img`).src = img2;
-        alreadychosen = true;
-    }else{
-        document.getElementById(`${Prio}Img`).src = img1;
-        alreadychosen = false;
-    }
+function changeImgOfPrio(Prio){
+    let img2 = `/assets/img/Prio${Prio}White.png`;
+    document.getElementById(`${Prio}Img`).src = img2;
 }
 
-/** changes the backgroundcolors of the Priority divs
- * @param {*} Prio 
- * @param {*} i 
+/**changes the backgroundcolor of the Prio-div
+ * 
+ * @param {string} Prio - changes the given number i to a Prioity in string-form
+ * @param {int} i - gives the function the Prio in int (1 = Urgent / 2 = Medium / 3 = Low Priority)
  */
 function changeColorOfPrio(Prio, i){
-    let img1 = `/assets/img/Prio${Prio}.png`;
-    let img2 = `/assets/img/Prio${Prio}White.png`;
-    document.getElementById(`Prio${i}`).classList.toggle(`${Prio}choosen`);
-    changeImgOfPrio(Prio, img1, img2);
+    document.getElementById(`Prio${i}`).classList.toggle(`${Prio}chosen`);
+    changeImgOfPrio(Prio);
 }
 
