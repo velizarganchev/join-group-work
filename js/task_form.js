275d9b13b39ccc2fbca40089fd7e2807ab8701a6
unknown <j.quabeck@gmx.net>
2024-01-17 20:57:30 +0100

    added id for task

--- js/task_form.js
+++ js/task_form.js
@@ -103,7 +103,7 @@ function cancelSubtask(){
  * this function adds new Subtasks to a List and to an Array
  */
 function addSubtask(){
-    let subtasks = [];
+    // let subtasks = [];
     let task = document.getElementById('subtask').value;
     document.getElementById('subTaskList').innerHTML += `<li>${task} <a class="deletebutton hover" type="text" onclick="deleteSubtask()">delete</a></li>`;
     allSubtasks.push(task);
@@ -119,7 +119,7 @@ function deleteSubtask(){
  * collects the values of all inputs
  */
 function getAllInputs(){ // Json auslagern -> funktioniert nicht..
-    let id = 1;
+    let id = allTasks.length +1; // Aufgaben dürfen erst aus allTasks gelöscht werden,wenn sie abgeschlossen sind!!
     let title = document.getElementById('title').value;
     let description = document.getElementById('description').value;
     let assignedTo = document.getElementById('assignedToSelect').value;
@@ -214,104 +214,6 @@ function addAnotherTask(){
 function flipTheImage(id){
     document.getElementById(`${id}`).classList.toggle('arrowdown');
     document.getElementById(`${id}`).classList.toggle('arrowup');
-}
-
-function getAllContacts(id){
-    flipTheImage(id);
-    document.getElementById('ContainerForAllPossibleContacts').classList.toggle('d-none');
-    generateContactsContainer();
-}
-
-function generateContactsContainer(){
-    let contactsContainer = document.getElementById('ContainerForAllPossibleContacts');
-    contactsContainer.innerHTML = '';
-    for (let i = 0; i < contacts.length; i++){
-        generateContactsCirle(contactsContainer, i);
-    }
-}
-
-function generateContactsCirle(contactsContainer, i){
-    let color = contacts[i]['color'];
-    let contact = contacts[i]['name'];
-    let firstLetterName = contact;
-    firstLetterName = firstLetterName.toUpperCase().slice(0,1);
-    let firstLetterLastName = contact.toUpperCase().slice(contact.lastIndexOf(' ')+1,contact.lastIndexOf(' ')+2);
-    let circle = generateCircle(i, firstLetterName, firstLetterLastName);
-    generateContactsHTML(contactsContainer, i, contact, color, circle);
-}
-
-function generateContactsHTML(contactsContainer, i, contact, color, circle){
-    contactsContainer.innerHTML += `
-    <div class="ContentContacts">
-        <div id="contactCircle${i}" class="contact hover">
-            ${circle}
-            <div>${contact}</div>
-        </div>
-        <div id="checkbox">
-            <input type="checkbox" id="checkboxFor${i}" class="checkboxes hover" onclick="safeContact(${i})">
-        </div>
-    </div>`;
-    document.getElementById(`circle${i}`).style = `background-color:${color}`;
-}
-
-function generateCircle(i, firstLetterName, firstLetterLastName){
-    return `
-    <div id="circle${i}" class="circle">${firstLetterName}${firstLetterLastName}</div>
-    `;
-}
-
-// let chosenContactsForTask = [];
-// function safeContact(id){
-//     let checkbox = document.getElementById(`checkboxFor${id}`).checked;
-//     let chosenContact = document.getElementById('ContainerForAllChosenContacts');
-//     if(checkbox){
-//         addChosenContact(id, chosenContact);
-//     }else{
-//         deletechosenContact(id, chosenContact);
-//     }
-// }
-
-// function addChosenContact(id, chosenContact){
-//     if(chosenContactsForTask.includes(id)){
-
-//     }
-//     else{
-//         chosenContactsForTask.push(id);
-//         chosenContact.innerHTML = '';
-//         for(let j = 0; j < chosenContactsForTask.length; j++){
-//             chosenContact.innerHTML += chosenContactsForTask[j];
-//         }
-//     }
-// }
-
-// function deletechosenContact(id, chosenContact){
-//     chosenContactsForTask.splice(id,1);
-//     chosenContact.innerHTML = '';
-//     for(let j = 0; j < chosenContactsForTask.length; j++){
-//         chosenContact.innerHTML += chosenContactsForTask[j];
-//     }
-// }
-
-function filterContacts(){
-    let search = document.getElementById('assignedToSelect').value;
-    search = search.toLowerCase();
-    let ContactList = document.getElementById('ContainerForAllPossibleContacts');
-    ContactList.innerHTML = '';
-    for(let i = 0; i < contacts.length; i++){
-        let name = contacts[i]['name'];
-        if (name.toLowerCase().includes(search)){
-            ContactList.innerHTML += `
-                <div class="ContentContacts">
-                    <div id="contactCircle${i}" class="contact hover">
-                        Kreis
-                        <div>${contacts[i]['name']}</div>
-                    </div>
-                    <div id="checkbox">
-                        <input type="checkbox" id="checkboxFor${i}" class="checkboxes hover" onclick="safeContact(${i})">
-                    </div>
-                </div>`;
-        }
-    }
 }
 
 /**
